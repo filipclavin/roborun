89aabd7a736135e448ab2d3ad4b0d22425457083
Filip Clavin <filipclavin99@gmail.com>
2023-10-18 18:15:30 +0200

    now possible to define allowed lanes for spawnables

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -2,12 +2,20 @@ using System;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
+[Flags]
+public enum Lanes
+{
+    Left = 1 << 0,
+    Middle = 1 << 1,
+    Right = 1 << 2
+}
 
 [Serializable]
 public struct SpawnableAddressable
 {
     public AssetReferenceT<GameObject> prefabAddressable;
     public float[] spawnHeights;
+    public Lanes allowedLanes;
 }
 
 [CreateAssetMenu(fileName = "GameData", menuName = "ScriptableObjects/GameData", order = 1)]
