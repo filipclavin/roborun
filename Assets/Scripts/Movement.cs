957cce8e403e0bef4feb908add3b1cc2b2a48cd2
Avocato1 <124534698+Avocato1@users.noreply.github.com>
2023-10-22 23:12:40 +0200

    Update
    Added animations and the option to adjust number of lanes in the inspector

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,108 +1,85 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using System.Collections;
 
 public class Movement : MonoBehaviour
 {
-    [Header("References")]
-    [SerializeField] private GameObject player;
-    private Rigidbody rb;
-    [Space]
     [Header("Movement")]
-    public float forwardSpeed = 10f;
-    public float jumpForce = 10f;
-    public float gravity = -20f;
-    public float slideTime = 1f;
-    [SerializeField] private float laneDistance = 4f;
-    [SerializeField] private float laneSwitchSpeed = 10f;
-    [SerializeField] private Animator animator;
-    
+    [Space]
+    [SerializeField]
+    private float jumpForce = 10f;
+    [SerializeField]
+    private float gravity = -20f;
+    [SerializeField]
+    private float laneWidth = 2f; 
+    [SerializeField]
+    private float laneSwitchSpeed = 10f;
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private int numberOfLanes = 5;
+    private int desiredLane;
+
+    private PlayerInput playerInput;
+    private Rigidbody rb;
+
     private Vector3 direction;
-    private int desiredLane = 1;
+
     private bool isJumping = false;
     private bool isSliding = false;
-    
-    private UIManager uiManager;
-
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
+        playerInput = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody>();
+        playerInput.actions["Slide"].performed += Slide;
+        desiredLane = numberOfLanes / 2;
+    }
+
+    private void OnDisable()
+    {
+        playerInput.actions["Slide"].performed -= Slide;
     }
 
     private void Update()
     {
-        
+        Animations();
         Physics.gravity = new Vector3(0, gravity, 0);
         GroundCheck();
-        MoveForward();
         MoveCharacter();
-    }
-
-    private void MoveForward()
-    {
-        rb.velocity = new Vector3(0, rb.velocity.y, forwardSpeed);
-        
     }
 
     private void MoveCharacter()
     {
         Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        
+        float lanePosition = (desiredLane - (numberOfLanes - 1) / 2.0f) * laneWidth;
 
-        switch (desiredLane)
-        {
-            case 0:
-                targetPosition += Vector3.left * laneDistance;
-                break;
-            case 2:
-                targetPosition += Vector3.right * laneDistance;
-                break;
-        }
+        targetPosition += transform.right * lanePosition;
 
         if (Time.timeScale != 0)
         {
             transform.position = Vector3.Lerp(transform.position, targetPosition, laneSwitchSpeed * Time.deltaTime / Time.timeScale);
         }
-
     }
 
     public void LaneTurn(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            if (context.ReadValue<float>() > 0)
-            {
-                desiredLane = Mathf.Clamp(desiredLane + 1, 0, 2);
-            }
-            else if (context.ReadValue<float>() < 0)
-            {
-                desiredLane = Mathf.Clamp(desiredLane - 1, 0, 2);
-            }
+            desiredLane = Mathf.Clamp(desiredLane + (int)context.ReadValue<float>(), 0, numberOfLanes - 1);
         }
     }
-
 
     public void Jump(InputAction.CallbackContext context)
     {
         if (context.performed && isGrounded)
         {
-            isJumping = true;
-            
             var velocity = rb.velocity;
             velocity = new Vector3(velocity.x, 0, velocity.z);
             rb.velocity = velocity;
 
-           rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            
-            animator.SetBool("isJumping", true);
-            animator.SetBool("isIdle", false);
-            animator.speed = 1 / Time.timeScale;
-        }
-        else
-        {
-            animator.SetBool("isIdle", true);
-            animator.SetBool("isJumping", false);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 
@@ -113,7 +90,7 @@ public class Movement : MonoBehaviour
         float distance = 1f;
         Vector3 dir = new Vector3(0, -1);
 
-        if(Physics.Raycast(transform.position, dir, out hit, distance))
+        if (Physics.Raycast(transform.position, dir, out hit, distance))
         {
             isGrounded = true;
         }
@@ -122,5 +99,38 @@ public class Movement : MonoBehaviour
             isGrounded = false;
         }
     }
-    
+
+    public void Slide(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            isSliding = true;
+        }
+        else
+        {
+            isSliding = false;
+        }
+    }
+
+    private void Animations()
+    {
+        if (rb.velocity.y > 0)
+        {
+            animator.SetBool("IsJumping", true);
+            animator.SetBool("IsRunning", false);
+            animator.SetBool("IsSliding", false);
+        }
+        else if (isSliding)
+        {
+            animator.SetBool("IsSliding", true);
+            animator.SetBool("IsRunning", false);
+            animator.SetBool("IsJumping", false);
+        }
+        else
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsRunning", true);
+            animator.SetBool("IsSliding", false);
+        }
+    }
 }
